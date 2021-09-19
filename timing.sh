c721dae9ec3a644a2511c674d05e67d61183c213
benjaminjsteenhoek@gmail.com <benjaminjsteenhoek@gmail.com>
2021-09-19 18:28:01 -0500

    Move all tools into subfolders

--- timing.sh
+++ timing.sh
@@ -1,20 +1,20 @@
 echo LLVM
 for i in $(seq 1 5); do
 echo execution $i
-time tools/llvm-example data/loop_exchange.c -- 2>&1 | ts -s '%.s'
+time llvm/llvm-example data/loop_exchange.c -- 2>&1 | ts -s '%.s'
 echo ''
 done
 
 echo Joern
 for i in $(seq 1 5); do
 echo execution $i
-time ~/bin/joern/joern-cli/joern --script tools/joern_example.sc --params inDirectory=data,inFile=data/loop_exchange.c 2>&1 | ts -s '%.s'
+time ~/bin/joern/joern-cli/joern --script joern/joern_example.sc --params inDirectory=data,inFile=data/loop_exchange.c 2>&1 | ts -s '%.s'
 echo ''
 done
 
 echo srcML
 for i in $(seq 1 5); do
 echo execution $i
-time python3 tools/srcml_example.py data/loop_exchange.c 2>&1 | ts -s '%.s'
+time python3 srcml/srcml_example.py data/loop_exchange.c 2>&1 | ts -s '%.s'
 echo ''
 done
